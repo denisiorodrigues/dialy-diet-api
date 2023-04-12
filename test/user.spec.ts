255e8a0f8ffe52ce4c3ac9eec482317318d28151
Denisio Rodrigues <denisio@ymail.com>
2023-04-12 05:43:26 -0300

    Escrevendo o teste e2e de usuário

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,2 +1,26 @@
-// import { describe } from 'vitest'
-// describre('', () => {})
+import { afterAll, beforeAll, describe } from 'vitest'
+import { app } from '../src/app'
+import { beforeEach } from 'node:test'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+
+describe('users routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('shold be able to create a new user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Esteban Tavares',
+    })
+  })
+})
